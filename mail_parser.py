cb84874384a9a9a28b9822fe84bf876860cee731
Vova <vova.plisko@mail.ru>
2023-08-11 17:25:34 +0700

    Switch to async module

--- mail_parser.py
+++ mail_parser.py
@@ -1,14 +1,11 @@
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 from config import login, psw
 from datetime import datetime
 
 mail_url = 'https://passport.yandex.ru/auth'
 folder_url = 'https://mail.yandex.ru/?uid=752034275#folder/22'
 
-
-
-
-def get_data():
+async def get_data():
     months = {'января': 1, #dictionary for convert data to datetime
                   'февраля': 2,
                   'марта': 3,
@@ -26,50 +23,50 @@ def get_data():
 
     year = datetime.now().year#number of current year for converting
 
-    with sync_playwright() as p:
+    async with async_playwright() as p:
         #login
-        browser = p.chromium.launch(headless= False)
-        page = browser.new_page()
-        page.goto(mail_url)
-        page.wait_for_timeout(2000)
-        page.click('text = Почта')
-        page.query_selector('[name = "login"]').fill(login)
-        page.click('id=passp:sign-in')
-        page.wait_for_timeout(1000)
-        page.query_selector('[name="passwd"]').fill(psw)
-        page.click('id=passp:sign-in')
-        page.wait_for_timeout(1500)
-        page.goto(folder_url)
-        page.wait_for_url(folder_url)
-        page.wait_for_timeout(1000)
+        browser =await p.chromium.launch(headless= False)
+        page =await browser.new_page()
+        await page.goto(mail_url)
+        await page.wait_for_url(mail_url)
+        await page.click('text = Почта')
+        await (await page.query_selector('[name = "login"]')).fill(login)
+        await page.click('id=passp:sign-in')
+        await page.wait_for_timeout(500)
+        await (await page.query_selector('[name="passwd"]')).fill(psw)
+        await page.click('id=passp:sign-in')
+        await page.wait_for_timeout(1000)
+        await page.goto(folder_url)
+        await page.wait_for_timeout(1000)
+        
 
         #load all messages for today
-        messages = page.query_selector_all('[class = mail-MessageSnippet-Item_dateText]')
-        while messages.pop().text_content().find(':') != -1:
-            page.query_selector('[class= _nb-button-content]').click()
-            messages = page.query_selector_all('[class = mail-MessageSnippet-Item_dateText]')
+        messages =await  page.query_selector_all('span[class = mail-MessageSnippet-Item_dateText]')
+        while (await messages.pop().text_content()).find(':') != -1:
+            await (await page.query_selector('[class= _nb-button-content]')).click()
+            messages = await page.query_selector_all('[class = mail-MessageSnippet-Item_dateText]')
 
         #find index of first message today
         for i in messages:
-            if  i.text_content().find(':') == -1 :
+            if  (await i.text_content()).find(':') == -1 :
                 index = messages.index(i)
                 break
-
+        index+=2
         #if noone msg sent today    
         if index == 0:
             return
         
         #get time and urls
-        pages  = page.query_selector_all('a[class = "mail-MessageSnippet js-message-snippet toggles-svgicon-on-important toggles-svgicon-on-unread"]')
+        pages  =await  page.query_selector_all('a[class = "mail-MessageSnippet js-message-snippet toggles-svgicon-on-important toggles-svgicon-on-unread"]')
         for i in range(index): 
-            pages[i].click()
-            page.wait_for_timeout(1000)
+            await pages[i].click()
+            await page.wait_for_timeout(1000)
             #url
-            url =page.query_selector('a[class = "46809b2d9d518540button-link"]').get_attribute('href')
+            url = await (await page.query_selector('a[class = "46809b2d9d518540button-link"]')).get_attribute('href')
 
             #time as list (need to convert to time format)
 
-            data = page.query_selector('div[class = "6943960f0ad08528event-info"]').text_content().split()
+            data = str(await (await page.query_selector('div[class = "6943960f0ad08528event-info"]')).text_content()).split()
             
             #convert list into datetime
             month = months[data[1]]
@@ -77,8 +74,9 @@ def get_data():
             t = data[2]
             data = datetime.strptime(f"{year}/{month}/{day} {t}","%Y/%m/%d %H:%M")
             
-            url_list.append([url,data])
+            url_list.append(str(url)+' '+str(data)+'\n')
 
-            page.go_back()
+            await page.go_back()
 
         return url_list
+    
--- main.py
+++ main.py
@@ -1,3 +1,37 @@
 import OBS
 import mail_parser
-import playwright
+import asyncio
+
+is_planned = 0
+
+async def parsing_stream():
+    lst = await mail_parser.get_data()
+
+    #filter urls that repits (now its like a set)
+    if type(lst) == list:
+        with open('list.txt','r') as f:
+            global_lst = f.readlines()
+        
+        for i in lst:
+            flag = 1
+            for line in global_lst:
+                if i.split(' ')[0]  in line:
+                    flag = 0
+            if flag:
+                global_lst.append(i)
+
+        with open('list.txt','w') as f:
+            f.writelines(global_lst)
+    await asyncio.sleep(3600) 
+
+async def recording_stream():
+    if(not is_planned):
+        #plan and change flag
+        pass
+
+
+async def main():
+    await asyncio.gather(parsing_stream(),recording_stream())
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
